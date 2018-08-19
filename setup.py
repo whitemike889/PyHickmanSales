7211c46c412f19b97fa11a70108fca5eb4706bec
Riley Flynn <riley@rileyflynn.me>
2018-08-19 13:01:01 -0230

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="PyHickmanSales",
+    version="1.0.0",
+    packages=["PyHickmanSales", ],
+    license="MIT",
+    description="Library providing access to sales listings from Hickman Chevrolet Cadillac",
+    url="https://github.com/nint8835/PyHickmanSales",
+    author="nint8835",
+    author_email="riley@rileyflynn.me",
+    install_requires=[
+        "lxml", "requests"
+    ]
+)
