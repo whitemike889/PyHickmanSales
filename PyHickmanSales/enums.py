c550ce33952461ff99201a5bae049b16361d472c
Riley Flynn <riley@rileyflynn.me>
2018-08-18 21:35:39 -0230

    Added enums

---
+++ PyHickmanSales/enums.py
@@ -0,0 +1,37 @@
+import enum
+
+
+class Locations(enum.Enum):
+    """
+    All Hickman Automotive Group dealerships.
+    """
+    BURIN = "gmcl-hickman-burninbay"
+    CARBONEAR = "gmcl-hickman-carbonear"
+    CLARENVILLE = "gmcl-hickman-clarenville"
+    CLARENVILLE_NISSAN = "nissan-hickman-en"
+    GANDER = "gmcl-hickman-gander"
+    GANDER_NISSAN = "nissan-hickmangander-en"
+    ST_JOHNS = "gmcl-hickman-en"
+    CHRYSLER = "motp-hickman-inv"
+    HONDA = "motp-hickmanhonda"
+    SELECT = "motp-hickman-inv1"
+
+
+class Category(enum.Enum):
+    """
+    Categories of vehicles sold.
+    """
+    NEW = "new"
+    DEMO = "demo"
+    USED = "preowned"
+
+
+class VehicleType(enum.Enum):
+    """
+    Types of vehicles sold.
+    """
+    CARGO_VAN = "CARGO VAN"
+    CAR = "Car"
+    SUV = "SUV"
+    TRUCK = "Truck"
+    VAN = "Van"
